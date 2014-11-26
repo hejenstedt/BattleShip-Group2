3b75ded1d4d03e8967ff421ee0ef2eb60417ad56
skaro87 <aschnackenburg@hotmail.com>
2014-11-26 13:32:25 +0100

    working on test class for Input

--- Test/ValidInputReturnsCharAndInt.java
+++ Test/ValidInputReturnsCharAndInt.java
@@ -1,13 +1,33 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import mainGame.InputFromPlayer;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class ValidInputReturnsCharAndInt {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	BufferedReader br;
+	
+	
+	@Before
+	public void createObjectsNeeded(){
+		br = new BufferedReader(new InputStreamReader(System.in));
 	}
+	
+	
+	@Test
+	public void testValidInput() {
+		InputFromPlayer input = new InputFromPlayer();
+		String userInput = input.getInput(br);
+		
+		assert(userInput.equals("a1"));
+		
+	}
+
 
 }
--- src/mainGame/InputFromPlayer.java
+++ src/mainGame/InputFromPlayer.java
@@ -7,35 +7,39 @@ public class InputFromPlayer {
 
 	private boolean validCoordinates;
 	private char validLetters[];
-	private int validNumbers[];
+	private char validNumbers[];
 
 	public InputFromPlayer() {
 		validLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 				'i', 'j' };
-		validNumbers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		validNumbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 	}
 
 	public String getInput(BufferedReader input) {
 		validCoordinates = false;
 		String inputFromUser = null;
-
 		while (!validCoordinates) {
+			
+			System.out.println("write coordinates:"); //TODO: Change text
+			
 			try {
 				inputFromUser = input.readLine().toLowerCase();
 			} catch (IOException e) {
 				System.out.println("can not get input");
 			}
 
-			if (correctLetter(inputFromUser.charAt(0))
-					&& correctNumber(inputFromUser.charAt(1)))
-
-				validCoordinates = true;
+			if (inputFromUser.length() == 2) {
+				if (this.correctLetter(inputFromUser.charAt(0))
+						&& this.correctNumber(inputFromUser.charAt(1))) {
+					validCoordinates = true;
+				}
+			}
 		}
 		return inputFromUser;
 
 	}
 
-	private boolean correctLetter(char inputAt0) {
+	public boolean correctLetter(char inputAt0) {
 		for (char i : validLetters) {
 			if (inputAt0 == i) {
 				return true;
@@ -44,8 +48,8 @@ public class InputFromPlayer {
 		return false;
 	}
 
-	private boolean correctNumber(int inputAt1) {
-		for (int i : validNumbers) {
+	public boolean correctNumber(char inputAt1) {
+		for (char i : validNumbers) {
 			if (inputAt1 == i) {
 				return true;
 			}
