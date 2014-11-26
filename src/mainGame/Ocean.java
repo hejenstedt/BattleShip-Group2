028b4ea91f7f2f57941c18006ec059160f87c3a4
hejenstedt <l.hejenstedt@gmail.com>
2014-11-26 11:27:01 +0100

    Tile class created, Ocean started

--- src/mainGame/Ocean.java
+++ src/mainGame/Ocean.java
@@ -2,4 +2,34 @@ package mainGame;
 
 public class Ocean {
 
+	private Tile[][] ocean;
+	
+	public Ocean() {
+		ocean = new Tile[10][10];
+		
+		for (int i = 0; i < ocean.length; i++) {
+			for (int j = 0; j < ocean[0].length; j++) {
+				ocean[i][j] = new Tile();
+			}
+		}
+		
+		
+	}
+	
+	public void placeBoat(int row, int column, int boatLength, int direction){
+		//TODO: 
+		
+	}
+
+	public void showOcean(){
+		
+		for (Tile[] tiles : ocean) {
+			for (Tile tile : tiles) {
+				
+				
+			}
+		}
+		
+	}
+	
 }
--- src/mainGame/Tile.java
+++ src/mainGame/Tile.java
@@ -2,4 +2,38 @@ package mainGame;
 
 public class Tile {
 
+	private boolean boatExistsOnTile;
+	private boolean tileHasBeenShootAt;
+	private Boat boat;
+
+	public Tile() {
+		tileHasBeenShootAt = false;
+		boatExistsOnTile = false;
+	}
+	
+	public void setBoatOnTile(Boat boat){
+		this.boat= boat;
+		boatExistsOnTile= true;
+	}
+
+	public void shootAtTile() {
+		tileHasBeenShootAt = true;
+	}
+
+	public boolean tileHasBeenShootBefore() {
+		return tileHasBeenShootAt;
+	}
+
+	@Override
+	public String toString() {
+		if (tileHasBeenShootAt) {
+
+			if (boatExistsOnTile) {
+				return boat.toString();
+			}
+			return "X";
+		}
+		return "~";
+	}
+
 }
