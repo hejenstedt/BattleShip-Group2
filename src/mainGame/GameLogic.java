c70c8127832782ae253ba2b965cdcb56be012e71
skaro87 <aschnackenburg@hotmail.com>
2014-11-27 12:20:40 +0100

    Made some logic

--- src/mainGame/GameLogic.java
+++ src/mainGame/GameLogic.java
@@ -1,5 +1,117 @@
 package mainGame;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class GameLogic {
+
+	BufferedReader br;
+	Ocean playerOcean;
+	Ocean aiOcean;
+	InputFromPlayer input;
+	AIlogic aiLogic;
+
+	public static void main(String[] args) {
+
+		GameLogic game = new GameLogic();
+		game.initializeGame(game);
+		game.runGame(game);
+
+	}
+
+	public void initializeGame(GameLogic game) {
+
+		br = new BufferedReader(new InputStreamReader(System.in));
+		playerOcean = new Ocean();
+		aiOcean = new Ocean();
+		input = new InputFromPlayer();
+		aiLogic = new AIlogic();
+
+	}
+
+	public void runGame(GameLogic game) {
+		// TODO: play again while loop here
+		game.placeShips();
+		game.playGame();
+
+		// TODO: clear grid from boats after game
+	}
+
+	public void placeShips() {
+		String boatCoordinatesString, boatDirection;
+		String boatNames [];
+		int boatCoorinatesInt[] = new int[2];
+		String shipType[] = new String [] {null, null, "destroyer", "cruiser", "battleship", "aircraft carrier"};
+		
+		for (int i = 5; i > 1; i--){
+			//TODO: while loop, validating ship has been placed
+			playerOcean.showOcean();
+			System.out.println("Where do you want to place your "+ shipType[i] + "? ("+i+")");
+
+			boatCoordinatesString = input.getInput(br, 1);
+			boatCoorinatesInt = input.changeCoordinatesToInt(boatCoordinatesString);
+			boatDirection = input.getDirection(br);
+			playerOcean.placeBoat(boatCoorinatesInt[0], boatCoorinatesInt[1], i,
+					boatDirection);
+			
+			if (i == 3){
+				System.out.println("Where do you want to place your sumbmarine? ("+i+")");
+				boatCoordinatesString = input.getInput(br, 1);
+				boatCoorinatesInt = input.changeCoordinatesToInt(boatCoordinatesString);
+				boatDirection = input.getDirection(br);
+				playerOcean.placeBoat(boatCoorinatesInt[0], boatCoorinatesInt[1], i,
+						boatDirection);
+			}
+		}
+
+		
+
+	
+
+		// TODO: AI ship placement logic
+	}
+
+	public void playGame() {
+		int currentPlayer = 1; // 1 = player, 0 = AI
+		String validInput;
+		int[] coordinates = new int[2];
+		boolean allBoatsShotDown = false;
+
+		while (!allBoatsShotDown) {
+			// currentPlayer++
+			// if (currentPlayer == 2){currentPlayer = 0}
+
+			if (currentPlayer == 1) { // get input from player
+				boolean coordinatesSelected = false;
+
+				while (!coordinatesSelected) {
+					System.out.println("Where do you want to shoot?");
+					validInput = input.getInput(br, 0);
+					coordinates = input.changeCoordinatesToInt(validInput);
+
+					if (validInput.equals("exit")) {
+						System.exit(0); // TODO: change?
+					} else if (validInput.equals("show")) {
+						// TODO: show player grid with boats
+					} else {
+						coordinates = input.changeCoordinatesToInt(validInput);
+						coordinatesSelected = true;
+					}
+
+				} // while coordinatesSelected
+
+				// TODO: change to AI ocean
+
+				playerOcean.shoot(coordinates[0], coordinates[1]);
+				playerOcean.showOcean();
+
+			}
+			if (currentPlayer == 0) {
+				// TODO: add AIlogic here
+			}
+
+		}
+
+	}
 
 }
--- src/mainGame/InputFromPlayer.java
+++ src/mainGame/InputFromPlayer.java
@@ -8,20 +8,53 @@ public class InputFromPlayer {
 	private boolean validCoordinates;
 	private char validLetters[];
 	private char validNumbers[];
+	private int coordinatesArray[];
+	private String keywords[];
 
 	public InputFromPlayer() {
 		validLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 				'i', 'j' };
-		validNumbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		validNumbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7',
+				'8', '9' };
+		coordinatesArray = new int[2];
+		keywords = new String[] { "exit", "show" };
 	}
 
-	public String getInput(BufferedReader input) {
+	public String getDirection(BufferedReader input) {
+		String direction = null;
+		boolean validDirection = false;
+
+		System.out.println("what direction? ('north', 'south', 'east' or 'west')");
+		
+		while (!validDirection) {
+
+			try {
+				direction = input.readLine().toLowerCase();
+			} catch (IOException e) {
+				System.out.println("can not get input");
+			}
+
+			if (direction.equals("north") || direction.equals("south")
+					|| direction.equals("east") || direction.equals("west")) {
+				validDirection = true;
+			} else {
+				System.out
+						.println("Invalid direction. Directions are: 'north', 'south', 'east' or 'west'");
+			}
+
+		}
+
+		return direction;
+	}
+
+	public String getInput(BufferedReader input, int inputType) {
 		validCoordinates = false;
 		String inputFromUser = null;
+
+		System.out.println("write coordinates:"); // TODO: Change text
+
 		while (!validCoordinates) {
-			
-			System.out.println("write coordinates:"); //TODO: Change text
-			
+
 			try {
 				inputFromUser = input.readLine().toLowerCase();
 			} catch (IOException e) {
@@ -33,13 +66,49 @@ public class InputFromPlayer {
 						&& this.correctNumber(inputFromUser.charAt(1))) {
 					validCoordinates = true;
 				}
+
+				else if (this.correctLetter(inputFromUser.charAt(1))
+						|| this.correctNumber(inputFromUser.charAt(0))) {
+					inputFromUser = "" + inputFromUser.charAt(1)
+							+ inputFromUser.charAt(0);
+					validCoordinates = true;
+				}
+
+				else {
+					System.out
+							.println("Invalid input. Coordinates are: 'letter' (A-J) and 'number' (0-9)"); // TODO:
+																											// change
+																											// text
+				}
+
 			}
+
+			else if (inputFromUser.equals(keywords[0])
+					|| inputFromUser.equals(keywords[1])) {
+				
+				if (inputType == 0){
+				System.out.println("Feature not yet added."); // TODO: remove
+																// this
+				// validCoordinates = true; TODO: remove comment, change
+				// keywords?
+				} else {
+					System.out
+					.println("Invalid input. Valid input is 'coordinates'");
+				}
+			}
+
+			else {
+				System.out
+						.println("Invalid input. Valid input is 'coordinates', '"
+								+ keywords[0] + "' or '" + keywords[1] + "'.");
+			}
+
 		}
 		return inputFromUser;
 
 	}
 
-	public boolean correctLetter(char inputAt0) {
+	private boolean correctLetter(char inputAt0) {
 		for (char i : validLetters) {
 			if (inputAt0 == i) {
 				return true;
@@ -48,13 +117,35 @@ public class InputFromPlayer {
 		return false;
 	}
 
-	public boolean correctNumber(char inputAt1) {
+	private boolean correctNumber(char inputAt1) {
 		for (char i : validNumbers) {
 			if (inputAt1 == i) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	public int[] changeCoordinatesToInt(String coordinates) {
+
+		int letterCount = 0, numberCount = 0;
+		for (char i : validLetters) {
+			if (coordinates.charAt(0) == i) {
+				break;
+			}
+			letterCount++;
+		}
+
+		for (char j : validNumbers) {
+			if (coordinates.charAt(1) == j) {
+				break;
+			}
+			numberCount++;
+		}
+
+		coordinatesArray[0] = numberCount;
+		coordinatesArray[1] = letterCount;
+		return coordinatesArray;
 	}
 
 }
