c8c7153b8d78014a7c2cd759d45273de102395b5
hejenstedt <l.hejenstedt@gmail.com>
2014-11-27 09:48:12 +0100

    moved a testclass in packages

---
+++ Test/mainGame/InputFromPlayerTest.java
@@ -0,0 +1,35 @@
+package mainGame;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import mainGame.InputFromPlayer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class InputFromPlayerTest {
+
+	BufferedReader br;
+	
+	
+	@Before
+	public void createObjectsNeeded(){
+		br = new BufferedReader(new InputStreamReader(System.in));
+	}
+	
+	
+	@Test
+	public void testValidInput() {
+		InputFromPlayer input = new InputFromPlayer();
+		String userInput = input.getInput(br);
+		
+		assert(userInput.equals("a1"));
+		
+	}
+
+	
+}
